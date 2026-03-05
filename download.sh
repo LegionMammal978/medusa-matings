64b9b2c5e8bc87174a3254f405d365f18cce2a9d
LegionMammal978 <mattlloydhouse@gmail.com>
2026-03-05 01:55:27 -0500

    Cygwin compatibility

--- download.sh
+++ download.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
+set -e
 rm -rf Medusa Medusa.tar.gz
-wget https://pi.math.cornell.edu/~dynamics/Matings/Medusa/Medusa.tar.gz
+curl -LO https://pi.math.cornell.edu/~dynamics/Matings/Medusa/Medusa.tar.gz
 tar -xvzf Medusa.tar.gz --exclude=Medusa/medusa_b.h # avoid name conflict on case-insensitive filesystems
 patch -d Medusa -p1 < fixes.patch
